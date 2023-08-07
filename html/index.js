fc70c7710556dd90af0de4d62a261c002aa57edc
shaikh irfan <heavysickboy@gmail.com>
2023-08-07 14:55:01 +0530

    first commit

---
+++ html/index.js
@@ -0,0 +1,41 @@
+ let x = "10";
+console.log(x);
+
+let y ="using dom manipulation";
+console.log([2]);
+console.log(y.lastIndexOf("i"));
+console.log(typeof Number(x));
+console.log(x.endsWith(0));
+console.log(y.substring(0,5));
+console.log(y.split("s"));
+console.log(y.replace("xn"," "));
+let heading = document.getElementById("myheading");
+heading.innerText = "use my dom";
+heading.style.backgroundColor = "red";
+heading.style.color="white";
+
+sum = 10;
+multi = 20;
+switch (sum){
+case 10:
+console.log(10);
+break;
+}
+for(let i=0;i<10;++i){
+console.log(Math.random());
+}
+ let z= 0;
+ while(z<10){
+    console.log("hii");
+    console.log("bye");
+z++;
+ }
+
+ function myfunction(a,b,c){
+    let name = "irfan"
+    console.log(`hi my name is ${name}`);
+console.log(a,b,c);
+  //  console.log(`i have three friends,${a},${b},${c}`)
+
+ }
+ myfunction(true,false,true);
